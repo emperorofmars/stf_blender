90dfc26a8213152db8b6fd704c79bc4ba82f5166
Mars <mars@squirrel.pub>
2025-05-23 22:18:41 +0200

    first clear blender material stf_material_to_blender

--- stfblender/modules_core/stf_material/convert_stf_material_to_blender.py
+++ stfblender/modules_core/stf_material/convert_stf_material_to_blender.py
@@ -2,7 +2,16 @@ import bpy
 
 
 def stf_material_to_blender(blender_material: bpy.types.Material):
-	pass
+	blender_material.use_nodes = True
+	output_node = blender_material.node_tree.get_output_node("ALL")
+	for node in blender_material.node_tree.nodes:
+		if(node != output_node):
+			blender_material.node_tree.nodes.remove(node)
+	# TODO select shader based on style hints and stuff
+	surface = blender_material.node_tree.nodes.new("ShaderNodeBsdfPrincipled")
+	blender_material.node_tree.links.new(surface.outputs[0], output_node.inputs[0])
+
+	# TODO parse stf material
 
 
 class STFConvertSTFMaterialToBlender(bpy.types.Operator):
