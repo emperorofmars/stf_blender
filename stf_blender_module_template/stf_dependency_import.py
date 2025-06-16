02bcd3f388c73149438e8fc4adf184cc6f6afcd4
Mars <mars@squirrel.pub>
2025-06-16 20:06:02 +0200

    module template fixup

--- stf_blender_module_template/stf_dependency_import.py
+++ stf_blender_module_template/stf_dependency_import.py
@@ -5,10 +5,12 @@ import addon_utils
 __installed_and_enabled_stf_extensions = []
 for m in {m.module for m in bpy.context.preferences.extensions.repos}:
 	loaded_default, loaded_state = addon_utils.check("bl_ext." + m + ".stf_blender")
-	if(loaded_state or m == "vscode_development"):
+	if(loaded_state and m == "vscode_development"):
 		__installed_and_enabled_stf_extensions.append(m)
-
-print(__installed_and_enabled_stf_extensions)
+	elif(loaded_state and m == "user_default"):
+		__installed_and_enabled_stf_extensions.append(m)
+	elif(loaded_state and m == "blender_org"):
+		__installed_and_enabled_stf_extensions.append(m)
 
 if("vscode_development" in __installed_and_enabled_stf_extensions):
 	print("Enabling Custom STF Modules for development extension.")
@@ -20,6 +22,7 @@ elif(len(__installed_and_enabled_stf_extensions) > 1):
 	print("Warning: STF Module detected more than one enabled STF extension! Using the first one from the '" + __installed_and_enabled_stf_extensions[0] + "' repository")
 	selected_stf_extension = "bl_ext." + __installed_and_enabled_stf_extensions[0] + ".stf_blender"
 else:
+	selected_stf_extension = None
 	print("No STF Extension detected. Not enabling Custom STF Modules!")
 
 if(selected_stf_extension):
