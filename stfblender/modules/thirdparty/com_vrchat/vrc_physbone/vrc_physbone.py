03d3e9c643c3a23965b87a8a198649962f960125
Mars <mars@squirrel.pub>
2025-06-22 21:14:22 +0200

    rudimentary vrc.physbone impl

--- stfblender/modules/thirdparty/com_vrchat/vrc_physbone/vrc_physbone.py
+++ stfblender/modules/thirdparty/com_vrchat/vrc_physbone/vrc_physbone.py
@@ -9,13 +9,93 @@ from .....utils.component_utils import STF_BlenderComponentBase, STF_BlenderComp
 _stf_type = "vrc.physbone"
 _blender_property_name = "vrc_physbone"
 
+# todo: this is quite jank, make this able to select the object/armature->bone/component etc..
+class PhysboneReference(bpy.types.PropertyGroup):
+	id: bpy.props.StringProperty(name="ID") # type: ignore
 
 class VRC_Physbone(STF_BlenderComponentBase):
-	# todo ignores, colliders, etc..
+	ignores: bpy.props.CollectionProperty(type=PhysboneReference, name="Ignored Children") # type: ignore
+	colliders: bpy.props.CollectionProperty(type=PhysboneReference, name="Colliders") # type: ignore
 	values: bpy.props.StringProperty(name="Json Values") # type: ignore
 
 
+class Edit_VRC_Physbone(bpy.types.Operator):
+	bl_idname = "stf.edit_vrc_physbone"
+	bl_label = "Edit"
+	bl_options = {"REGISTER", "UNDO"}
+
+	component_id: bpy.props.StringProperty() # type: ignore
+	blender_bone: bpy.props.BoolProperty() # type: ignore
+
+	op: bpy.props.StringProperty() # type: ignore
+	property: bpy.props.StringProperty() # type: ignore
+	index: bpy.props.IntProperty() # type: ignore
+
+	def execute(self, context):
+		if(self.op == "add"):
+			if(not self.blender_bone):
+				for pb in context.object.vrc_physbone:
+					if(pb.stf_id == self.component_id):
+						getattr(pb, self.property).add()
+						return {"FINISHED"}
+			else:
+				for pb in context.bone.vrc_physbone:
+					if(pb.stf_id == self.component_id):
+						getattr(pb, self.property).add()
+						return {"FINISHED"}
+		elif(self.op == "remove"):
+			if(not self.blender_bone):
+				for pb in context.object.vrc_physbone:
+					if(pb.stf_id == self.component_id):
+						getattr(pb, self.property).remove(self.index)
+						return {"FINISHED"}
+			else:
+				for pb in context.bone.vrc_physbone:
+					if(pb.stf_id == self.component_id):
+						getattr(pb, self.property).remove(self.index)
+						return {"FINISHED"}
+		self.report({"ERROR"}, "Couldn't edit Physbone")
+		return {"CANCELLED"}
+
+
 def _draw_component(layout: bpy.types.UILayout, context: bpy.types.Context, component_ref: STF_Component_Ref, context_object: any, component: VRC_Physbone):
+	box = layout.box()
+
+	row = box.row()
+	row.label(text="Colliders")
+	add_button = row.operator(Edit_VRC_Physbone.bl_idname, text="Add")
+	add_button.blender_bone = type(component.id_data) == bpy.types.Armature
+	add_button.component_id = component.stf_id
+	add_button.op = "add"
+	add_button.property = "colliders"
+	
+	for index, collider in enumerate(component.colliders):
+		row = box.row()
+		row.prop(collider, "id")
+		add_button = row.operator(Edit_VRC_Physbone.bl_idname, text="", icon="X")
+		add_button.blender_bone = type(component.id_data) == bpy.types.Armature
+		add_button.component_id = component.stf_id
+		add_button.op = "remove"
+		add_button.index = index
+		add_button.property = "colliders"
+
+	row = box.row()
+	row.label(text="Ignores")
+	add_button = row.operator(Edit_VRC_Physbone.bl_idname, text="Add")
+	add_button.blender_bone = type(component.id_data) == bpy.types.Armature
+	add_button.component_id = component.stf_id
+	add_button.op = "add"
+	add_button.property = "ignores"
+	for index, ignore in enumerate(component.ignores):
+		row = box.row()
+		row.prop(ignore, "id")
+		add_button = row.operator(Edit_VRC_Physbone.bl_idname, text="", icon="X")
+		add_button.blender_bone = type(component.id_data) == bpy.types.Armature
+		add_button.component_id = component.stf_id
+		add_button.op = "remove"
+		add_button.index = index
+		add_button.property = "ignores"
+
 	layout.prop(component, "values")
 
 
@@ -23,15 +103,33 @@ def _stf_import(context: STF_ImportContext, json_resource: dict, stf_id: str, co
 	component_ref, component = add_component(context_object, _blender_property_name, stf_id, _stf_type)
 	import_component_base(component, json_resource)
 	component.values = json.dumps(json_resource["values"])
-	# todo ignores, colliders, etc..
+
+	for ignore in json_resource.get("ignores", []):
+		new_ignore = component.ignores.add()
+		new_ignore.id = ignore
+
+	for collider in json_resource.get("colliders", []):
+		new_collider = component.colliders.add()
+		new_collider.id = collider
+
 	return component
 
 
-def _stf_export(context: STF_ExportContext, application_object: VRC_Physbone, context_object: any) -> tuple[dict, str]:
-	ret = export_component_base(_stf_type, application_object)
-	ret["values"] = application_object.values
-	# todo ignores, colliders, etc..
-	return ret, application_object.stf_id
+def _stf_export(context: STF_ExportContext, component: VRC_Physbone, context_object: any) -> tuple[dict, str]:
+	ret = export_component_base(_stf_type, component)
+	ret["values"] = component.values
+
+	ignores = []
+	for ignore in component.ignores:
+		ignores.append(ignore.id)
+	ret["ignores"] = ignores
+
+	colliders = []
+	for collider in component.colliders:
+		colliders.append(collider.id)
+	ret["colliders"] = colliders
+
+	return ret, component.stf_id
 
 
 class STF_Module_VRC_Physbone(STF_BlenderComponentModule):
