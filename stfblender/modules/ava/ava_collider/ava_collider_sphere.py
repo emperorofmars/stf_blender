2f2feb1803d334ec8733a4c5fa76537dc8466784
Mars <mars@squirrel.pub>
2025-06-23 18:43:47 +0200

    experiment with json pasting

--- stfblender/modules/ava/ava_collider/ava_collider_sphere.py
+++ stfblender/modules/ava/ava_collider/ava_collider_sphere.py
@@ -1,5 +1,6 @@
 import bpy
 import mathutils
+import json
 
 from ....exporter.stf_export_context import STF_ExportContext
 from ....importer.stf_import_context import STF_ImportContext
@@ -16,9 +17,57 @@ class AVA_Collider_Sphere(STF_BlenderComponentBase):
 	offset_position: bpy.props.FloatVectorProperty(name="Position Offset", size=3, default=(0, 0, 0), subtype="XYZ") # type: ignore
 
 
+class AVA_Collider_Sphere_LoadJsonOperator(bpy.types.Operator):
+	bl_idname = "stf.ava_collider_sphere_loadjson"
+	bl_label = "Set from Json"
+	bl_options = {"REGISTER", "UNDO"}
+
+	component_id: bpy.props.StringProperty() # type: ignore
+	blender_bone: bpy.props.BoolProperty() # type: ignore
+
+	json_string: bpy.props.StringProperty() # type: ignore
+
+	def invoke(self, context, event):
+		return context.window_manager.invoke_props_dialog(self)
+
+	def execute(self, context):
+		try:
+			json_resource = json.loads(self.json_string)
+			if(not self.blender_bone):
+				for component in context.object.ava_collider_sphere:
+					if(component.stf_id == self.component_id):
+						break
+			else:
+				for component in context.bone.ava_collider_sphere:
+					if(component.stf_id == self.component_id):
+						break
+			if(json_resource.get("type") == _stf_type and component):
+				component.radius = json_resource.get("radius", 1)
+				if("offset_position" in json_resource):
+					offset_position = mathutils.Vector()
+					for index in range(3):
+						offset_position[index] = json_resource["offset_position"][index]
+					component.offset_position = stf_translation_to_blender(offset_position)
+				return {"FINISHED"}
+		except:
+			pass
+		self.report({"ERROR"}, "Failed applying json values.")
+		return {"CANCELLED"}
+
+	def draw(self, context):
+		layout: bpy.types.UILayout = self.layout
+		layout.label(text="Paste json setup string.")
+		layout.label(text="(This will overwrite your current values)")
+		layout.prop(self, "json_string", text="")
+
+
 def _draw_component(layout: bpy.types.UILayout, context: bpy.types.Context, component_ref: STF_Component_Ref, context_object: any, component: AVA_Collider_Sphere):
 	layout.prop(component, "radius")
 	layout.prop(component, "offset_position")
+	
+	load_json_button = layout.operator(AVA_Collider_Sphere_LoadJsonOperator.bl_idname)
+	load_json_button.blender_bone = type(component.id_data) == bpy.types.Armature
+	load_json_button.component_id = component.stf_id
 
 
 def _stf_import(context: STF_ImportContext, json_resource: dict, stf_id: str, context_object: any) -> any:
