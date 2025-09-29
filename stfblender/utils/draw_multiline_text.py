494eabadd1db821ed8afac86908e8def67a54d36
Mars <mars@squirrel.pub>
2025-09-29 22:58:19 +0200

    textwrap

--- stfblender/utils/draw_multiline_text.py
+++ stfblender/utils/draw_multiline_text.py
@@ -1,14 +1,12 @@
 import bpy
+import textwrap
 
-def draw_multiline_text(layout: bpy.types.UILayout, text: str):
+def draw_multiline_text(layout: bpy.types.UILayout, text: str, width: int = 80):
+	wrap = textwrap.TextWrapper(width=width)
 	row = layout.row()
 	row.column().label(text="", icon="INFO_LARGE")
 	col_r = row.column()
 	if(not text.endswith(".")):
 		text += "."
-	for line in text.split("\n"): # Todo more intelligent splitting
-		remaining = line
-		while(len(remaining) > 80):
-			col_r.label(text=remaining[:80])
-			remaining = remaining[80:]
-		col_r.label(text=remaining)
+	for line in wrap.wrap(text=text):
+		col_r.label(text=line)
