a7173786f27e00ce84e2bf2c36279cd9aae17f2d
Mars <mars@squirrel.pub>
2025-10-22 23:19:22 +0200

    minor simplification

--- testsuite/tests/test_basic_import_export.py
+++ testsuite/tests/test_basic_import_export.py
@@ -7,18 +7,14 @@ class TestBasicImportExport(unittest.TestCase):
 	def test_import(self):
 		try:
 			ret = bpy.ops.stf.import_files("EXEC_DEFAULT", directory="testsuite/assets", files=[{"name":"default_cube.stf"}])
-			for r in ret:
-				self.assertTrue(r == "FINISHED")
-				break
+			self.assertTrue("FINISHED" in ret)
 		except Exception as e:
 			self.fail(str(e))
 
 	def test_export(self):
 		try:
 			ret = bpy.ops.stf.export("EXEC_DEFAULT", filepath="testsuite/output/ret.stf", debug=True)
-			for r in ret:
-				self.assertTrue(r == "FINISHED")
-				break
+			self.assertTrue("FINISHED" in ret)
 		except Exception as e:
 			self.fail(str(e))
 
