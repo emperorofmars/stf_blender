507b1ab17748b9fa66a04f1046e1a81a7f4308ea
Mars <mars@squirrel.pub>
2026-03-05 20:20:40 +0100

    fixed module template

--- stf_blender_module_template/template_stf_component_module.py
+++ stf_blender_module_template/template_stf_component_module.py
@@ -17,8 +17,8 @@ def _draw_component(layout: bpy.types.UILayout, context: bpy.types.Context, comp
 
 
 def _stf_import(context: stfblender.common.STF_ImportContext, json_resource: dict, stf_id: str, context_object: Any) -> Any:
-	component_ref, component = stfblender.common.module_component.component_utils.add_component(context_object, _blender_property_name, stf_id, _stf_type)
-	ret = stfblender.common.module_component.component_utils.import_component_base(context, component, json_resource, _blender_property_name, context_object)
+	component_ref, component = stfblender.common.resource.component.component_utils.add_component(context_object, _blender_property_name, stf_id, _stf_type)
+	ret = stfblender.common.resource.component.component_utils.import_component_base(context, component, json_resource, _blender_property_name, context_object)
 
 	component.squeak = json_resource.get("squeak", True)
 
@@ -26,12 +26,12 @@ def _stf_import(context: stfblender.common.STF_ImportContext, json_resource: dic
 
 
 def _stf_export(context: stfblender.common.STF_ExportContext, component: SqueakComponent, context_object: Any) -> tuple[dict, str]:
-	ret = stfblender.common.module_component.utils.component_utils.export_component_base(context, _stf_type, component, _blender_property_name, context_object)
+	ret = stfblender.common.resource.component.component_utils.export_component_base(context, _stf_type, component, _blender_property_name, context_object)
 	ret["squeak"] = component.squeak
 	return ret, component.stf_id
 
 
-class MyCustomSTFSqueakComponentModule(stfblender.common.resource.STF_Handler_Component):
+class MyCustomSTFSqueakComponentHandler(stfblender.common.resource.STF_Handler_Component):
 	stf_type = _stf_type
 	stf_category = "component"
 	understood_application_types = [SqueakComponent]
@@ -46,7 +46,7 @@ class MyCustomSTFSqueakComponentModule(stfblender.common.resource.STF_Handler_Co
 	like_types = ["squeak"]
 
 
-register_stf_handlers = [MyCustomSTFSqueakComponentModule]
+register_stf_handlers = [MyCustomSTFSqueakComponentHandler]
 
 
 def register():
